7a2bdf4b5aa46d765345e56d481913fbcf68a720
slynch16 <73437652+slynch16@users.noreply.github.com>
2021-03-23 20:13:32 -0400

    Complete project

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const cat = {
+    name : ['Mittens', 'Louis', 'Pukey'],
+    desire : ['I am hungry.', 'I threw up on your bed.' , 'I want to cuddle.'],
+    toDo : ['Give me a treat!', 'Brush my belly!' , 'Get the stain remover!']
+    
+}
 
+let randomNumber  = (number) => {
+    return Math.floor(Math.random() * number);
+};
+
+let catNeeds = [];
+
+for (item in cat){
+    let index = randomNumber(cat[item].length);
+
+    switch (item){
+        case 'name':
+        catNeeds.push(`My name is ${cat[item][index]}.`);
+        break;
+        case 'desire':
+        catNeeds.push(`${cat[item][index]}`);
+        break;
+        case 'toDo':
+        catNeeds.push(`${cat[item][index]}`);
+        break;
+    };
+};
+
+
+    function formatCat (speak){
+        const formatted = speak.join(' ');
+        console.log(formatted);
+    };
+
+    console.log(formatCat(catNeeds));
